7a1d30cff1a4bcfb5e4222944bb11897f2f91466
Jordyn Bonds <jbonds@gmail.com>
2014-12-06 15:29:28 -0500

    mvp if we're just recreating tosdr :)

--- Project/js/scripts.js
+++ Project/js/scripts.js
@@ -4,7 +4,45 @@ $(document).ready(function(){
 		if (e.which == 13) {
 			var service = $('#search').val();
 			$.getJSON('https://tosdr.org/api/1/service/'+service+'.json').done(function(data){
-	  			$('#result').text(JSON.stringify(data),undefined,3);
+	  			$.each(data.pointsData,function(){
+	  				if ( this.tosdr.point === "good" ){
+	  					$('#results').append('' + 
+							'<div class="result">' + 
+            					'<h2>' + 
+                					'<span class="label label-success"><span class="glyphicon glyphicon-ok"></span></span>' +
+                					this.title +
+                				'</h2>' +
+                				'<p>' +
+                					this.tosdr.tldr +
+                				'</p>' +
+                			'</div>' +
+	  					'');
+	  				} else if ( this.tosdr.point === "bad" ){
+	  					$('#results').append('' + 
+							'<div class="result">' + 
+            					'<h2>' + 
+                					'<span class="label label-warning"><span class="glyphicon glyphicon-warning-sign"></span></span>' +
+                					this.title +
+                				'</h2>' +
+                				'<p>' +
+                					this.tosdr.tldr +
+                				'</p>' +
+                			'</div>' +
+	  					'');
+	  				} else {
+	  					$('#results').append('' + 
+							'<div class="result">' + 
+            					'<h2>' + 
+                					'<span class="label label-danger"><span class="glyphicon glyphicon-remove"></span></span>' +
+                					this.title +
+                				'</h2>' +
+                				'<p>' +
+                					this.tosdr.tldr +
+                				'</p>' +
+                			'</div>' +
+	  					'');
+	  				}
+	  			})
 			});
 			return false;
 		}
